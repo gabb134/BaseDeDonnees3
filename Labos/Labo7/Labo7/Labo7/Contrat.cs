15f66fd5a133fb07f66995745a28a3cfa18917f7
gabb134 <cg.marrero@cgodin.qc.ca>
2019-10-17 07:44:28 -0400

    modif gab ordi mac 17 oct 2019

---
+++ Labos/Labo7/Labo7/Labo7/Contrat.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labo7
+{
+    class Contrat
+    {
+
+        public int intNumero { get; private set; }
+        public int intMontant { get; private set; }
+        public  int intNumEmploye { get; private set; }
+
+
+        public Contrat()
+        {
+
+        }
+
+        public Contrat(int intNumero,int intMontant,int intNumEmploye)
+        {
+            this.intNumero = intNumero;
+            this.intMontant = intMontant;
+            this.intNumEmploye = intNumEmploye;
+        }
+
+
+    }
+}
---
+++ Labos/Labo7/Labo7/Labo7/Employe.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labo7
+{
+    class Employe
+    {
+
+       public int intNumero { get; private set; }
+       public string strPrenom { get; private set; }
+       public string strNom { get; private set; }
+       public int intSalaire { get; private set; }
+    
+
+
+        public Employe()
+        {
+
+        }
+
+        public Employe(int intNumero,string strPrenom,string strNom,int intSalaire)
+        {
+            this.intNumero = intNumero;
+            this.strPrenom = strPrenom;
+            this.strNom = strNom;
+            this.intSalaire = intSalaire;
+        }
+
+        static void main(string[] args)
+        {
+
+        }
+    }
+}
