431b5de8a6e0753f51cc92356615f0dd36b47468
cg.marrero@cgodin.qc.ca <cg.marrero@cgodin.qc.ca>
2019-10-31 15:01:14 -0400

    modif gab 31 oct 2019

---
+++ Labos/testReport/testReport/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace testReport
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            Microsoft.Reporting.WinForms.ReportDataSource reportDataSource1 = new Microsoft.Reporting.WinForms.ReportDataSource();
+            this.reportViewer1 = new Microsoft.Reporting.WinForms.ReportViewer();
+            this.BDVoyagesMarreroDataSet = new testReport.BDVoyagesMarreroDataSet();
+            this.voyageBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.voyageTableAdapter = new testReport.BDVoyagesMarreroDataSetTableAdapters.voyageTableAdapter();
+            ((System.ComponentModel.ISupportInitialize)(this.BDVoyagesMarreroDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.voyageBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // reportViewer1
+            // 
+            reportDataSource1.Name = "DataSet1";
+            reportDataSource1.Value = this.voyageBindingSource;
+            this.reportViewer1.LocalReport.DataSources.Add(reportDataSource1);
+            this.reportViewer1.LocalReport.ReportEmbeddedResource = "testReport.Report1.rdlc";
+            this.reportViewer1.Location = new System.Drawing.Point(144, 51);
+            this.reportViewer1.Name = "reportViewer1";
+            this.reportViewer1.ServerReport.BearerToken = null;
+            this.reportViewer1.Size = new System.Drawing.Size(396, 246);
+            this.reportViewer1.TabIndex = 0;
+            // 
+            // BDVoyagesMarreroDataSet
+            // 
+            this.BDVoyagesMarreroDataSet.DataSetName = "BDVoyagesMarreroDataSet";
+            this.BDVoyagesMarreroDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // voyageBindingSource
+            // 
+            this.voyageBindingSource.DataMember = "voyage";
+            this.voyageBindingSource.DataSource = this.BDVoyagesMarreroDataSet;
+            // 
+            // voyageTableAdapter
+            // 
+            this.voyageTableAdapter.ClearBeforeFill = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.reportViewer1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.BDVoyagesMarreroDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.voyageBindingSource)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Microsoft.Reporting.WinForms.ReportViewer reportViewer1;
+        private System.Windows.Forms.BindingSource voyageBindingSource;
+        private BDVoyagesMarreroDataSet BDVoyagesMarreroDataSet;
+        private BDVoyagesMarreroDataSetTableAdapters.voyageTableAdapter voyageTableAdapter;
+    }
+}
+
---
+++ Labos/testReport/testReport/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace testReport
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: cette ligne de code charge les données dans la table 'BDVoyagesMarreroDataSet.voyage'. Vous pouvez la déplacer ou la supprimer selon les besoins.
+            this.voyageTableAdapter.Fill(this.BDVoyagesMarreroDataSet.voyage);
+
+            this.reportViewer1.RefreshReport();
+        }
+    }
+}
