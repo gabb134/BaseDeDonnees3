66f73683ef3533b1e96cae2329b70ed6ea72b926
cg.marrero@cgodin.qc.ca <cg.marrero@cgodin.qc.ca>
2019-11-01 13:04:33 -0400

    modif gab ordi 444 1 nov 2019

---
+++ Labos/Labo11/Labo11/Labo11/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Labo11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            XDocument docCollege = XDocument.Load("college.xml");
+
+            /*a) Affichez les noms de tous les départements du collège.
+
+
+
+            //Console.WriteLine(docCollege);
+            var tousLesNomsDepartement = from unCollege in docCollege.Elements("college")
+                                     from unDepartement in unCollege.Elements("departement")
+                                     select unDepartement.Attribute("nom");
+
+
+            Console.WriteLine("requete A");
+
+            foreach (XAttribute unNomDepartement in tousLesNomsDepartement)
+                Console.WriteLine(unNomDepartement);
+
+            Console.ReadKey();
+
+
+            /*********************************************************/
+            //b) Tous les éléments « noemploye » qu’il y a dans le
+            //collège en ordre de numéro d’employé.
+
+            var tousLesElementsNoEmploye = from unCollege in docCollege.Elements("college")
+                                           from unDepartement in unCollege.Elements("departement")
+                                           from unEnseignent in unDepartement.Elements("enseignant")
+                                           select unEnseignent.Elements("noemploye");
+
+            Console.WriteLine("requete B");
+
+            foreach (var unNoEmploye in tousLesElementsNoEmploye)
+                Console.WriteLine(unNoEmploye);
+
+            Console.ReadKey();
+
+
+            /*********************************************************/
+
+        }
+    }
+}
